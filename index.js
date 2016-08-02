e16ce4edeb1f10317355085f39cdb522ff0742f1
FrancescoSTL <francescosapolizzi@gmail.com>
2016-08-02 15:09:33 -0700

    Add copy, styling

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var self = require("sdk/self");
-let { Cc, Ci, Cu } = require('chrome');
 var windowUtils = require('sdk/window/utils');
-//Cu.import('chrome://browser/content/browser-places.js');
+
+// get the active window
 var activeWindow = windowUtils.getMostRecentBrowserWindow();
+
+// get the bookmark menu from our window
 var menu = activeWindow.document.getElementById("editBookmarkPanel");
 
+// create and style our hr linebreak
+var horizLine = activeWindow.document.createElement("hr");
+horizLine.setAttribute("style", "margin-top:20px; height:1px; background-color:grey");
+
+// create our content body
+var paragraph = activeWindow.document.createElement("p");
+paragraph.setAttribute("style", "margin-top:15px;");
 var textnode = activeWindow.document.createTextNode("Access your bookmarks on the go! Download Firefox for iOS/Android.");
-menu.appendChild(textnode);
+
+// append our elements together
+menu.appendChild(horizLine);
+paragraph.appendChild(textnode);
+menu.appendChild(paragraph);
