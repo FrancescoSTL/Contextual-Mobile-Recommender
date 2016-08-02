11ce84328c2fe612e5e881e2dde044d98f4581cc
FrancescoSTL <francescosapolizzi@gmail.com>
2016-08-02 12:19:57 -0700

    Simple append to bookmark UI

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var self = require("sdk/self");
+let { Cc, Ci, Cu } = require('chrome');
+var windowUtils = require('sdk/window/utils');
+//Cu.import('chrome://browser/content/browser-places.js');
+var activeWindow = windowUtils.getMostRecentBrowserWindow();
+var menu = activeWindow.document.getElementById("editBookmarkPanel");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
-
-exports.dummy = dummy;
+var textnode = activeWindow.document.createTextNode("Access your bookmarks on the go! Download Firefox for iOS/Android.");
+menu.appendChild(textnode);
